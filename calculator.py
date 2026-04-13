9a14dcc4bb3c3748a6a58a6b6f0f001620f24a52
kontkara <yildizsefercan@gmail.com>
2026-04-13 21:53:43 +0300

    Improved type hinting and docs

--- calculator.py
+++ calculator.py
@@ -1,9 +1,11 @@
-def check_result(func: callable, *args: Union[Tuple[Union[int, float], ...]]) -> None:
+def check_result(func: callable, *args: Union[Tuple[Union[int, float], ...], None]) -> None:
     try:
         result = func(*args)
         if isinstance(result, (int, float)):
             print(f"Result: {result}")
-        else:
+        elif result is not None:
             print("Non-numeric result")
+    except TypeError as e:
+        print(f"TypeError: {e}")
     except Exception as e:
         print(f"Error: {e}")
