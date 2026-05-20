08ea282c0404262dffb6ae22d15223727279c913
kontkara <yildizsefercan@gmail.com>
2026-05-20 09:35:20 +0300

    Cleaned up redundant code

--- calculator.py
+++ calculator.py
@@ -1,29 +1,13 @@
 def process_and_call_wrapper(func: Callable[[*Any], *Any]) -> Union[Callable[[*Any], *Any], None]:
     ...
-    if check_builtin(func):  # exclude built-in functions
-        pass
-    elif isinstance(func, str):
-        wrapped_func = safe_eval(get_lambda_name(func))
-    elif callable(func) and not check_builtin(func):  
+    if callable(func) and not check_builtin(func):  
         try:
             result = func(*processed_args)
             validate_return_type(result)
             wrapped_func = lambda *args: wrapper(*args) if len(args) == len(processed_arg_names) else None
         except TypeError as e:
             raise ValueError(f"Incorrect type of arguments for {func.__name__}") from e
-    else:
-        return None
-
-    if wrapped_func is not None and callable(wrapped_func):
-        def with_logging(*args, **kwargs):
-            logging.info("Calling function")
-            return wrapped_func(*args, **kwargs)
-        return typing.cast(Union[Callable[[*Any], *Any], None], wrap_with_logging(with_logging))
-
-    if processed_args is not None:
-        processed_args = tuple(arg if isinstance(arg, tuple) else (arg,) for arg in process_and_check_for_none(tuple(map(lambda x: x if x is not None else (), processed_args))))
-    
-    return typing.cast(Union[Callable[[*Any], *Any], None], wrapped_func)
+    ...
 
 def process_and_check_for_none(args):
     result = []
@@ -33,5 +17,8 @@ def process_and_check_for_none(args):
         elif isinstance(arg, tuple):
             result.append(tuple(process_and_check_for_none(item) for item in arg))
         else:
-            result.append(arg)
+            if isinstance(arg, (int, str)):
+                result.append(arg)
+            else:
+                result.append(tuple((arg,)))
     return typing.cast(tuple, tuple(result))
