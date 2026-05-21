3b65e0ca8c7a1d3e9fa4d57580a872ef4d480e9a
kontkara <yildizsefercan@gmail.com>
2026-05-21 15:33:24 +0300

    Optimized main functions

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+from typing import Any, Callable, Union
+
 def process_and_call_wrapper(func: Callable[[*Any], *Any]) -> Union[Callable[[*Any], *Any], None]:
     ...
     if callable(func) and not check_builtin(func):  
@@ -7,7 +9,7 @@ def process_and_call_wrapper(func: Callable[[*Any], *Any]) -> Union[Callable[[*A
             wrapped_func = lambda *args: wrapper(*args) if len(args) == len(processed_arg_names) else None
         except TypeError as e:
             raise ValueError(f"Incorrect type of arguments for {func.__name__}") from e
-    ...
+    return wrapped_func
 
 def process_and_call_wrapper_with_hint(func: Callable[[*Any], *Any]) -> Union[Callable[[*Any], *Any], None]:
-    return typing.cast(Union[Callable[[*Any], *Any], None], process_and_call_wrapper(func))
+    return process_and_call_wrapper(func)
